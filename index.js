cf414e35c5d1e5347505cb5b2a2c1183100fa082
Riley <riley.childs@yahoo.com>
2025-06-22 07:57:48 -0400

    Init commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+const port = 3000 || process.env.PORT
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`)
+})
