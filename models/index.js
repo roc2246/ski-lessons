ad75771f4a98a8e1a852c1ae8143f6e1f9c0e444
Riley <riley.childs@yahoo.com>
2025-06-29 19:00:33 -0400

    organized code in newUser()

--- models/index.js
+++ models/index.js
@@ -23,27 +23,38 @@ async function dbConnect() {
 
 async function newUser(username, password) {
   try {
+    // INPUT VALIDATION
+    if (username === " ") throw new Error("Username required");
+    if (password === " ") throw new Error("Password required");
+
+    // CONNECT
     await dbConnect();
 
+    // CREATE SCHEMA AND MODEL
     const userSchema = new Schema({
       username: { type: String, required: true, unique: true },
       password: { type: String, required: true },
     });
-
     const UserModel =
       mongoose.models.User || mongoose.model("User", userSchema);
 
+      // CHECK IF USER IS IN DB
+    const userInDB = await UserModel.find({ username });
+    if (userInDB.length > 0) throw new Error("User already exists");
+
+    // HASH PASSWORD
     const hashedPassword = await bcrypt.hash(password, 12);
 
+    // CREATE NEW USER
     const newUser = new UserModel({ username, password: hashedPassword });
-
     await newUser.save();
-
     console.log("User successfully created");
   } catch (error) {
     console.error("Error creating user:", error);
     await errorEmail("Failed to register user", error.toString());
     throw error;
+  } finally{
+     mongoose.disconnect();
   }
 }
 
