6543ee55eb2417c037d9c758c1ae2f1d32df234e
Riley <riley.childs@yahoo.com>
2025-07-09 18:22:29 -0400

    Created utility function for user models

--- utilities/index.js
+++ utilities/index.js
@@ -1,11 +1,19 @@
+import mongoose from "mongoose";
+
 function argValidation(args, argNames) {
   for (let i = 0; i < args.length; i++) {
-    if (!args[i]) {
-      throw new Error(`${argNames[i]} required`);
-    }
+    if (!args[i]) throw new Error(`${argNames[i]} required`);
   }
 }
 
+function getUserModel(schemaDefinition, modelName) {
+  argValidation([schemaDefinition, modelName], ["schemaDefinition", "modelName"])
+  const schema = new mongoose.Schema(schemaDefinition);
+  console.log("schema created")
+  return/*  mongoose.models[modelName] || */ mongoose.model(modelName, schema);
+}
+
 module.exports = {
-argValidation
-} 
+  argValidation,
+  getUserModel
+};
