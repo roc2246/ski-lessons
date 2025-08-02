ca8970142281178c5eec5159e9e20e1d8727ba25
Riley <riley.childs@yahoo.com>
2025-08-02 17:54:41 -0400

    Reorganized code in lesson-board.js

--- public/scripts/lesson-board.js
+++ public/scripts/lesson-board.js
@@ -55,6 +55,30 @@ function preprocessLessons(lessons) {
   });
 }
 
+async function assignLesson(lessonId, token) {
+  try {
+    const response = await fetch(`/api/lessons/${lessonId}/assign`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const data = await response.json();
+    console.log(data);
+    if (!response.ok) {
+      throw new Error(data?.message || "Failed to assign lesson");
+    }
+
+    console.log("Lesson assignment updated:", data.lesson);
+    return data.lesson;
+  } catch (error) {
+    console.error("Error assigning lesson:", error.message);
+    throw error;
+  }
+}
+
 async function renderCalendar(date) {
   const year = date.getFullYear();
   const month = date.getMonth();
@@ -134,30 +158,3 @@ function changeMonth(offset) {
 
 // Initial render
 renderCalendar(currentDate);
-
-// ___________________________________________________
-async function assignLesson(lessonId, token) {
-  try {
-    const response = await fetch(`/api/lessons/${lessonId}/assign`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    const data = await response.json();
-    console.log(data);
-    if (!response.ok) {
-      throw new Error(data?.message || "Failed to assign lesson");
-    }
-
-    console.log("Lesson assignment updated:", data.lesson);
-    return data.lesson;
-  } catch (error) {
-    console.error("Error assigning lesson:", error.message);
-    throw error;
-  }
-}
-
-// (async () => await assignLesson("687ceb9ed32791c6f61af55e", token))();
