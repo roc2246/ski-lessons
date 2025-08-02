0a18fb3cea878fd94532bb80046804d856113625
Riley <riley.childs@yahoo.com>
2025-08-02 17:17:37 -0400

    Updated lessons schema

--- utilities/index.js
+++ utilities/index.js
@@ -73,6 +73,7 @@ export function schemas() {
       timeLength: { type: String, required: true },
       guests: { type: Number, required: true },
       assignedTo: { type: String, required: true, ref: "User" }, // fixed this too
+      upForGrabs: { type: Boolean, required: true}, // fixed this too
     }),
   };
 }
