5088e8b27450b6e2ed2fe1845c8d81c6b7cbc57a
Riley <riley.childs@yahoo.com>
2025-11-30 20:14:15 -0700

    Instructors can now populate dropdown for create lesson form

--- public/scripts/create-lesson.js
+++ public/scripts/create-lesson.js
@@ -1,29 +1,42 @@
 import * as lib from "./admin-library.js";
 
-      // -----------------------------
-      // FORM SUBMIT HANDLER
-      // -----------------------------
-      const form = document.getElementById("lesson-form");
+// -----------------------------
+// FORM SUBMIT HANDLER
+// -----------------------------
+const form = document.getElementById("lesson-form");
+(async () => {
+  try {
+    const dropdown = document.getElementById("assignedTo");
+    const users = await lib.getUsers();
+    for (let x = 0; x < users.length; x++) {
+      const opt = document.createElement("option");
+      opt.value = users[x]._id
+      opt.innerText = users[x].username;
+      dropdown.appendChild(opt);
+    }
+  } catch (err) {
+    console.error("Failed to fetch users:", err);
+  }
+})();
 
-      form.addEventListener("submit", async (e) => {
-        e.preventDefault();
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const lessonData = {
+    type: document.getElementById("type").value,
+    date: document.getElementById("date").value,
+    timeLength: document.getElementById("timeLength").value,
+    guests: document.getElementById("guests").value,
+    assignedTo: document.getElementById("assignedTo").value,
+  };
 
-        const lessonData = {
-          type: document.getElementById("type").value,
-          date: document.getElementById("date").value,
-          timeLength: document.getElementById("timeLength").value,
-          guests: document.getElementById("guests").value,
-          assignedTo: document.getElementById("assignedTo").value,
-        };
+  try {
+    const createdLesson = await lib.lessonCreate(lessonData);
+    alert("Lesson created successfully!");
+    console.log(createdLesson);
 
-        try {
-          const createdLesson = await lib.lessonCreate(lessonData);
-          alert("Lesson created successfully!");
-          console.log(createdLesson);
-
-          form.reset();
-        } catch (err) {
-          console.error(err);
-          alert("Failed to create lesson.");
-        }
-      });
+    form.reset();
+  } catch (err) {
+    console.error(err);
+    alert("Failed to create lesson.");
+  }
+});
