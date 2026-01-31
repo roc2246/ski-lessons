ac558ad315d13e75d0b1659265950565be377fa8
Riley <riley.childs@yahoo.com>
2026-01-30 20:10:52 -0700

    Set env variable for production vs development

--- server/index.js
+++ server/index.js
@@ -21,16 +21,14 @@ app.use(express.json());
 app.use("/api", routes);
 
 // Serve React build
-app.use(
-  express.static(path.join(__dirname, "../client/dist"))
-);
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
 // React router fallback
-app.get("*", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "../client/dist/index.html")
-  );
-});
+if (process.env.NODE_ENV === "production") {
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  });
+}
 
 // Error handler (optional but fine)
 app.use((err, req, res, next) => {
