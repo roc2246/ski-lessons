11353901222ab078242a816d81f932e78bf6af49
Riley <riley.childs@yahoo.com>
2026-02-04 17:51:27 -0700

    Lessons can now toggle on click

--- client/src/components/calendar-dir/CalendarDate.jsx
+++ client/src/components/calendar-dir/CalendarDate.jsx
@@ -3,20 +3,26 @@ import Lesson from "../Lesson";
 
 function CalendarDate({ day, lessons = [], onLessonAdded }) {
   const [localLessons, setLocalLessons] = useState([]);
+  const [showLessons, setShowLessons] = useState(false);
 
   useEffect(() => {
     setLocalLessons(lessons);
   }, [lessons]);
 
-
+  const handleClick = () => {
+    if (localLessons.length > 0) {
+      setShowLessons((prev) => !prev); // toggle on click
+    }
+  };
 
   return (
-    <div className="calendar__date">
+    <div className="calendar__date" onClick={handleClick} style={{ cursor: localLessons.length > 0 ? "pointer" : "default" }}>
       <span>{day.getDate()}</span>
 
-      {localLessons.map((lesson) => (
-        <Lesson key={lesson._id} lesson={lesson} />
-      ))}
+      {showLessons &&
+        localLessons.map((lesson) => (
+          <Lesson key={lesson._id} lesson={lesson} />
+        ))}
     </div>
   );
 }
