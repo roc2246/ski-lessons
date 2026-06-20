fa485bfa52dc7f0cd43da7c1c2186ff948228d31
Riley <riley.childs@yahoo.com>
2026-06-20 06:54:24 -0400

    Fixed lesson date bug

--- client/src/components/calendar-dir/CalendarDates.jsx
+++ client/src/components/calendar-dir/CalendarDates.jsx
@@ -1,21 +1,50 @@
 import React from "react";
 import CalendarDate from "./CalendarDate";
 
-export default function CalendarDates({ currentDate, lessons = [], onAddLesson }) {
-  // Generate all dates for the current month
-  const dates = Array.from({ length: new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate() }, (_, i) =>
-    new Date(currentDate.getFullYear(), currentDate.getMonth(), i + 1)
+function parseLocalDate(dateString) {
+  const [year, month, day] = dateString.split("-").map(Number);
+  return new Date(year, month - 1, day);
+}
+
+function isSameCalendarDay(dateA, dateB) {
+  return (
+    dateA.getFullYear() === dateB.getFullYear() &&
+    dateA.getMonth() === dateB.getMonth() &&
+    dateA.getDate() === dateB.getDate()
+  );
+}
+
+export default function CalendarDates({
+  currentDate,
+  lessons = [],
+  onAddLesson,
+}) {
+  const dates = Array.from(
+    {
+      length: new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth() + 1,
+        0
+      ).getDate(),
+    },
+    (_, i) => new Date(currentDate.getFullYear(), currentDate.getMonth(), i + 1)
   );
 
   return (
     <div className="calendar__dates">
       {dates.map((day) => {
-        const lessonsForDay = lessons.filter(
-          (lesson) => new Date(lesson.date).toDateString() === day.toDateString()
-        );
+        const lessonsForDay = lessons.filter((lesson) => {
+          const lessonDate = parseLocalDate(lesson.date);
+          return isSameCalendarDay(lessonDate, day);
+        });
 
         return (
-          <CalendarDate key={day.toISOString()} day={day} lessons={lessonsForDay} onAddLesson={onAddLesson} />
+          <CalendarDate
+            key={`${day.getFullYear()}-${day.getMonth()}-${day.getDate()}`}
+            day={day}
+            lessons={lessonsForDay}
+            onAddLesson={onAddLesson}
+          />
         );
       })}
     </div>
